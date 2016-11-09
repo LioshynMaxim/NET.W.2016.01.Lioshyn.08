a54b178461b19ff48adb44e8f08b05be69a6ea14
Maksim Lioshyn <leshinmaxim@gmail.com>
2016-11-09 07:50:22 +0300

    Day8. Complete 75%

---
+++ Task1/Customer.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Customer : IFormattable
+    {
+
+        public string Name { get; private set; }
+        public string ContactPhone { get; private set; }
+        public decimal Revenue { get; private set; }
+
+        /// <summary>
+        /// .ctor
+        /// </summary>
+        /// <param name="name">Name value</param>
+        /// <param name="phone">Contact phone value</param>
+        /// <param name="revenue">Revenue value</param>
+
+        public Customer(string name, string phone, decimal revenue)
+        {
+            Name = name;
+            ContactPhone = phone;
+            Revenue = revenue;
+        }
+
+        /// <summary>
+        /// Override ToString in invariant Culture
+        /// </summary>
+        /// <param name="format">format show classes</param>
+        /// <returns>String line in invariant Culture</returns>
+
+        public string ToString(string format)
+        {
+            return ToString(format, CultureInfo.InvariantCulture);
+        }
+
+        /// <summary>
+        /// Override ToString in invariant Culture
+        /// </summary>
+        /// <param name="format">format show classes</param>
+        /// <param name="formatProvider"></param>
+        /// <returns>String line in invariant Culture</returns>
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            if (format == null)
+                throw new ArgumentNullException();
+
+            format = format.ToUpperInvariant();
+
+            switch (format)
+            {
+                case "":
+                    return Name;
+                case "NAME":
+                    return Name;
+                case "PHONE":
+                    return ContactPhone;
+                case "REVENUE":
+                    return Revenue.ToString("F");
+
+                case "NP":
+                    return Name + " " + ContactPhone;
+                case "NR":
+                    return Name + " " + Revenue.ToString("F");
+
+                //case "PN":
+                //    return ContactPhone + " " + Name;
+                //case "PR":
+                //    return ContactPhone + " " + Revenue.ToString("F");
+
+                case "RN":
+                    return Revenue.ToString("C") + " " + Name;
+                case "RP":
+                    return Revenue.ToString("C") + " " + ContactPhone;
+
+                case "NPR":
+                    return Name + " " + ContactPhone + " " + Revenue.ToString("F");
+
+                default:
+                    throw new FormatException($"Bad format {format}. Atatata");
+            }
+        }
+    }
+}
---
+++ Task1/CustomerFormatter.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class CustomerFormatter : IFormatProvider, IFormattable
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public IFormatProvider ParentProvider { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parentProvider"></param>
+
+        public CustomerFormatter(IFormatProvider parentProvider)
+        {
+            this.ParentProvider = parentProvider;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public CustomerFormatter() : this(CultureInfo.InvariantCulture)
+        {
+
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="formatType"></param>
+        /// <returns></returns>
+
+        public object GetFormat(Type formatType)
+        {
+            if (formatType == typeof(ICustomFormatter))
+                return this;
+            return null;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="format"></param>
+        /// <param name="obj"></param>
+        /// <param name="formatProvider"></param>
+        /// <returns></returns>
+
+        public string ToString(string format, object obj, IFormatProvider formatProvider)
+        {
+            if (format == null || obj == null)
+                throw new ArgumentNullException();
+
+            format = format.ToUpperInvariant();
+
+            Customer customer = obj as Customer; //????????
+
+            switch (format)
+            {
+                case "PN":
+                    return customer?.ContactPhone + " " + customer?.Name;
+                case "PR":
+                    return customer?.ContactPhone + " " + customer?.Revenue.ToString("F");
+                default: throw new FormatException($"Bad format {format}. Atatata");
+            }
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
